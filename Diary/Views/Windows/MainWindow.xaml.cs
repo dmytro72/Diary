bbb2c4dfada3fc6d1a11e18f4e4a38be434eee24
Dmytro Shaban <10shaban72@gmail.com>
2021-06-04 15:15:37 +0300

    Implement SearchTask

--- Diary/Views/Windows/MainWindow.xaml.cs
+++ Diary/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using Diary.Models;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Diary
 {
@@ -7,6 +11,28 @@ namespace Diary
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TasksCollectionFilter(object sender, System.Windows.Data.FilterEventArgs e)
+        {
+            if (!(e.Item is Task task)) return;
+            if (task.Place is null) return;
+            if (task.Name is null) return;
+
+            var filter_text = TaskPlaceFilterText.Text;
+            if (filter_text.Length == 0) return;
+
+            if (task.Place.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (task.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnTasksFilterTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("TasksCollection");
+            collection.View.Refresh();
         }
     }
 }
